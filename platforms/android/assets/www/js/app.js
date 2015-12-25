b458a6de15cd748a4c6a08c9ab2ccf4c5d5b92b5
oliver002 <oliver.oreshkovski@hotmail.com>
2015-12-25 13:46:27 +0100

    add platforms and plugins

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,37 @@
+  	 function playVideo(){
+      //window.plugins.streamingMedia.playVideo("http://80.77.157.70/abc/hd.m3u8");
+      var videoUrl = "http://80.77.157.70/abc/hd.m3u8";
+      // Just play a video
+      //window.plugins.streamingMedia.playVideo(videoUrl);
+      // Play a video with callbacks
+      var options = {
+        successCallback: function() {
+          console.log("Video was closed without error.");
+          //alert("Video was closed without error.");
+        },
+        errorCallback: function(errMsg) {
+          console.log("Error! " + errMsg);
+          alert("Streaming is not available right now!");
+        }
+      };
+      window.plugins.streamingMedia.playVideo(videoUrl, options);
+    }
+
+(function() {
+  angular.module('starter', ['ionic','ngCordova'])
+	.run(['$ionicPlatform','$rootScope','$state', function($ionicPlatform, $rootScope, $state) {
+		$ionicPlatform.ready(function() {
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+			// for form inputs)
+			if(window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			}
+			if(window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+		});
+		//$rootScope.goHome = function() {
+		//	$state.go("Entries");
+		//};
+	}]);
+}());
---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,119 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.hutchind.cordova.plugins.streamingmedia/www/StreamingMedia.js",
+        "id": "com.hutchind.cordova.plugins.streamingmedia.StreamingMedia",
+        "pluginId": "com.hutchind.cordova.plugins.streamingmedia",
+        "clobbers": [
+            "streamingMedia"
+        ]
+    },
+    {
+        "file": "plugins/com.hutchind.cordova.plugins.vitamio/www/Vitamio.js",
+        "id": "com.hutchind.cordova.plugins.vitamio.Vitamio",
+        "pluginId": "com.hutchind.cordova.plugins.vitamio",
+        "clobbers": [
+            "vitamio"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.hutchind.cordova.plugins.streamingmedia": "0.1.4",
+    "com.hutchind.cordova.plugins.vitamio": "0.0.2",
+    "cordova-plugin-console": "1.0.2",
+    "cordova-plugin-device": "1.1.0",
+    "cordova-plugin-dialogs": "1.2.0",
+    "cordova-plugin-inappbrowser": "1.1.0",
+    "cordova-plugin-network-information": "1.1.1-dev",
+    "cordova-plugin-splashscreen": "3.0.0",
+    "cordova-plugin-statusbar": "2.0.0",
+    "cordova-plugin-whitelist": "1.2.0",
+    "ionic-plugin-keyboard": "1.0.8"
+}
+// BOTTOM OF METADATA
+});
